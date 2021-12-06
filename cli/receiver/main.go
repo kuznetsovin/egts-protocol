92b231c1c9b7da4cc004213fe1aaad9aa96f81e5
Igor Kuznetsov <kuznetsovin@gmail.com>
2021-12-06 19:25:42 +0300

    feat: add cli config params

--- cli/receiver/main.go
+++ cli/receiver/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"flag"
 	"github.com/kuznetsovin/egts-protocol/cli/receiver/config"
 	"github.com/kuznetsovin/egts-protocol/cli/receiver/server"
 	"github.com/kuznetsovin/egts-protocol/cli/receiver/storage"
 	log "github.com/sirupsen/logrus"
-	"os"
 	"plugin"
 )
 
@@ -14,13 +14,17 @@ func main() {
 		store storage.Connector
 	)
 
-	if len(os.Args) != 2 {
+	cfgFilePath := ""
+	flag.StringVar(&cfgFilePath, "c", "", "Конфигурационный файл")
+	flag.Parse()
+
+	if cfgFilePath == "" {
 		log.Fatalf("Не задан путь до конфига")
 	}
 
-	cfg, err := config.New(os.Args[1])
+	cfg, err := config.New(cfgFilePath)
 	if err != nil {
-		log.Fatalf("Ошибка парсинга конфига: %v", err)
+		log.Fatal("Ошибка парсинга конфига: %v", err)
 	}
 
 	log.SetLevel(cfg.GetLogLevel())
